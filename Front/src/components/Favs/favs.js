51fbbc5502671abb6def5d5be96141a5ffe40162
JhonDaniel01 <jhon-coso@hotmail.com>
2023-05-04 23:18:05 -0500

    Repair

--- Front/src/components/Favs/favs.js
+++ Front/src/components/Favs/favs.js
@@ -5,25 +5,25 @@ import { useEffect } from 'react';
 
 
 export default function Favs(props) {
-   const characters = useSelector((state) => state.characters);
-   const dispatch = useDispatch();
+   // //const characters = useSelector((state) => state.characters);
+   // const dispatch = useDispatch();
    
-   useEffect(() => {
-      //dispatch(getCharacters());
-    }, []);
+   // useEffect(() => {
+   //    //dispatch(getCharacters());
+   //  }, []);
 
-   return ( <div className={style.contenedorCard}>
-      {characters.map(character=>{
-          return <Card
-          key={character.id}
-          id={character.id}
-          name={character.name}
-          species={character.species}
-          gender={character.gender}
-          image={character.image}
-          onClose={props.onClose}
-        />
-      }
-      )}
-   </div>);
+   // return ( <div className={style.contenedorCard}>
+   //    {characters.map(character=>{
+   //        return <Card
+   //        key={character.id}
+   //        id={character.id}
+   //        name={character.name}
+   //        species={character.species}
+   //        gender={character.gender}
+   //        image={character.image}
+   //        onClose={props.onClose}
+   //      />
+   //    }
+   //    )}
+   // </div>);
 }
